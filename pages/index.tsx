6db26df7bed1bc1a18feafb07e103bfd561e7a30
kristjangrm <kristjan.grm1@gmail.com>
2021-12-04 15:27:33 +0100

    providers can be now added through a PR

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,75 +2,86 @@ import type { NextPage } from "next";
 import Head from 'next/head';
 import DataTable from 'react-data-table-component';
 import { useEffect, useState } from 'react';
+import { promises as fs } from 'fs'
+import path from 'path'
 
 export interface DataRow {
-  provider : string
-  min_amount : string
-  max_amount : string
-  fee : string
-  rate : string
-  pair : string
-  url : string
+  provider: string
+  min_amount: string
+  max_amount: string
+  fee: string
+  rate: string
+  pair: string
+  url: string
+  name: string
 }
 
-const Home: NextPage = () => {
+const Home: NextPage = (props : any) => {
 
   const [tableData, setTableData] = useState<DataRow[]>([]);
 
   useEffect(() => {
+    console.log(props.providers);
     getData();
-}, [])
+  }, [])
 
   const getData = () => {
-    fetch('https://lnsov.ml:9001/getpairs')
-      .then(response => response.json())
-      .then(data => {
-        let pairs = data.pairs;
-        Object.entries(pairs).map(([key, value] : any) => {
-          setTableData(tableData => [...tableData,{
-            fee : value.fees.percentage + "%",
-            max_amount : value.limits.maximal,
-            min_amount : value.limits.minimal,
-            pair : key,
-            provider : "https://lnsov.vercel.app/",
-            rate : value.rate,
-            url : "https://lnsov.ml:9001",
-          }] );
-      });
-    });
-  }
+    props.providers.forEach(provider => {
+      fetch(provider.getPairUrl)
+        .then(response => response.json())
+        .then(data => {
+          let pairs = data.pairs;
+          Object.entries(pairs).map(([key, value]: any) => {
+            setTableData(tableData => [...tableData, {
+              fee: value.fees.percentage + "%",
+              max_amount: value.limits.maximal,
+              min_amount: value.limits.minimal,
+              pair: key,
+              provider: provider.publicUrl,
+              rate: value.rate,
+              url: provider.getPairUrl,
+              name: provider.name
+            }]);
+          });
+        });
+    })
+  };
 
   const columns = [
     {
-      name: 'provider',
-      selector: (row: DataRow ) => row.provider,
+      name: 'Provider',
+      cell: (row: DataRow) => <a className="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800" href={row.provider}>{row.name}</a>,
       button: true,
       wrap: true,
-      minWidth: '160px',
+      minWidth: '200px',
     },
     {
       name: 'Fee',
       selector: (row: DataRow) => row.fee,
+      maxWidth: '30px',
+      sortable: true,
     },
     {
       name: 'Max amount',
       selector: (row: DataRow) => row.max_amount,
+      sortable: true,
     },
     {
       name: 'Min amount',
-      selector:  (row: DataRow) => row.min_amount,
+      selector: (row: DataRow) => row.min_amount,
+      sortable: true,
     },
     {
-      name: 'pair',
+      name: 'Pair',
       selector: (row: DataRow) => row.pair,
+      maxWidth: '30px',
+      sortable: true,
     },
     {
-      name: 'rate',
-      selector:  (row: DataRow) => row.rate,
-    },
-    {
-      name: 'url',
-      selector:  (row: DataRow) => row.url,
+      name: 'Rate',
+      selector: (row: DataRow) => row.rate,
+      maxWidth: '200px',
+      sortable: true,
     }
   ];
 
@@ -87,15 +98,13 @@ const Home: NextPage = () => {
           Lightning bridge marketplace
         </h1>
 
-        <p className="text-xl mt-2 text-center">
+        <p className="max-w-5xl text-xl mt-2 text-center">
           This website is collection of Lightning bridges. It is a place where you can find all the information about the bridges. You can also find the bridge you are looking for.
         </p>
 
-        { tableData != undefined && <DataTable
-          columns={columns}
-          data={tableData}
-        />
-        }
+        <div className="max-w-5xl m-auto">
+          <DataTable  columns={columns} data={tableData} highlightOnHover />
+        </div>
 
 
       </main>
@@ -104,6 +113,23 @@ const Home: NextPage = () => {
       </footer>
     </div>
   )
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps(): Promise<{ props: { providers: any[] } }> {
+  const filePath = path.join(process.cwd(), 'bridge-providers.json');
+  const fileContents = await fs.readFile(filePath, 'utf8');
+  var providers = JSON.parse(fileContents);
+
+  // By returning { props: { providers } }, the Home component
+  // will receive `providers` as a prop at build time
+  return {
+    props: {
+      providers: await Promise.all(providers),
+    },
+  }
 }
 
 export default Home
